7d942fd3be310f3b7f5e2f1372b97a19bac65fb6
naman2103 <naman.gupta@splashlearn.com>
2021-06-14 10:12:01 +0530

    aggregate query over incoming stream

--- adaptive-learning/SlidingWindow/src/main/java/com/amazonaws/services/kinesisanalytics/SlidingWindowStreamingJobWithParallelism.java
+++ adaptive-learning/SlidingWindow/src/main/java/com/amazonaws/services/kinesisanalytics/SlidingWindowStreamingJobWithParallelism.java
@@ -2,6 +2,7 @@ package com.amazonaws.services.kinesisanalytics;
 
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,12 +12,12 @@ import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisConsumer;
 import org.apache.flink.streaming.connectors.kinesis.FlinkKinesisProducer;
 import org.apache.flink.streaming.connectors.kinesis.config.ConsumerConfigConstants;
-
+import org.apache.flink.api.common.functions.AggregateFunction;
 import java.util.Properties;
 
 /**
- * A Kinesis Data Analytics for Java application that calculates minimum stock
- * price for all stock symbols in a given Kinesis stream over a sliding window
+ * A Kinesis Data Analytics for Java application that calculates average time per question and
+ * accuracy for all attempted questions by a user in a given Kinesis stream over a sliding window
  * and overrides the operator level parallelism in the flink application.
  * <p>
  * Note that the maximum parallelism in the Flink code cannot be greater than
@@ -32,7 +33,7 @@ import java.util.Properties;
  *      --current-application-version-id <VersionId>
  *      --application-configuration-update "{\"FlinkApplicationConfigurationUpdate\": { \"ParallelismConfigurationUpdate\": {\"ParallelismUpdate\": 5, \"ConfigurationTypeUpdate\": \"CUSTOM\" }}}"
  */
-public class SlidingWindowStreamingJobWithParallelism {
+public class SlidingWindowStreamingJobWithParallelism{
     private static final String region = "us-west-2";
     private static final String inputStreamName = "cdc-postgrey-test";
     private static final String outputStreamName = "cdc-test-output-stream";
@@ -60,15 +61,60 @@ public class SlidingWindowStreamingJobWithParallelism {
         ObjectMapper jsonParser = new ObjectMapper();
         input.map(value -> { // Parse the JSON
             JsonNode jsonNode = jsonParser.readValue(value, JsonNode.class);
-            return new Tuple2<>(jsonNode.get("user_id").asText(), jsonNode.get("time_spent").asDouble());
+            return new Tuple3<>(jsonNode.get("user_id").asText(), jsonNode.get("time_spent").asDouble(), jsonNode.get("is_correct").asBoolean());
         })
-                .returns(Types.TUPLE(Types.STRING, Types.DOUBLE))
+                .returns(Types.TUPLE(Types.STRING, Types.DOUBLE, Types.BOOLEAN))
                 .keyBy(value->value.f0) // Logically partition the stream per user_id
                 .timeWindow(Time.seconds(10), Time.seconds(5)) // Sliding window definition
-                .sum(1) // Calculate sum of time_taken per question over the window
-                .setParallelism(2) // Set parallelism for the sum operator
-                .map(value -> "sum of time_taken over a time window : " + value.f0 +" : " + value.f1.toString() + "\n")
+                .aggregate(new AverageAggregator())
+//                .print();
+                .setParallelism(1)
+                .map(value -> " CHECKING - " + value+ "\n")
                 .addSink(createSinkFromStaticConfig());
+
         env.execute("sum of time_spent");
+    }
+
+    public static class AverageAggregator implements AggregateFunction<Tuple3<String, Double, Boolean>, MyAverage, Tuple2<MyAverage, Double>>{
+        @Override
+        public MyAverage createAccumulator(){
+            return new MyAverage();
+        }
+
+        @Override
+        public MyAverage add(Tuple3<String, Double, Boolean> in, MyAverage myAverage) {
+            myAverage.variant = in.f0;
+            myAverage.count = myAverage.count + 1;
+            myAverage.sum = myAverage.sum + in.f1;
+            return myAverage;
+        }
+
+        @Override
+        public Tuple2<MyAverage, Double> getResult(MyAverage myAverage) {
+            return new Tuple2<>(myAverage, myAverage.sum / myAverage.count);
+        }
+
+        @Override
+        public MyAverage merge(MyAverage myAverage, MyAverage acc1) {
+            myAverage.sum = myAverage.sum + acc1.sum;
+            myAverage.count = myAverage.count + acc1.count;
+            return myAverage;
+        }
+    }
+
+    public static class MyAverage {
+
+        public String variant;
+        public Integer count = 0;
+        public Double sum = 0d;
+
+        @Override
+        public String toString() {
+            return "MyAverage{" +
+                    "variant='" + variant + '\'' +
+                    ", count=" + count +
+                    ", sum=" + sum +
+                    '}';
+        }
     }
 }
